0218ff9df661fe3d343087f21dafe4bcaaa5244f
Alexander <34252505+0x0BSoD@users.noreply.github.com>
2019-07-31 13:22:54 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,10 +11,9 @@ using namespace std;
 int main(int argc, _TCHAR* argv[])
 {
 	Search  sr;
-	Updates upd;
+	Updates updates;
 	HRESULT hr;
-
-	string str_param;
+	BSTR criteria;
 	hr = CoInitialize(NULL);
 	
 	// Download
@@ -24,37 +23,15 @@ int main(int argc, _TCHAR* argv[])
 	IUpdateCollection* ToDownloadList;
 
 	hr = CoCreateInstance(CLSID_UpdateCollection, NULL, CLSCTX_INPROC_SERVER, IID_IUpdateCollection, (LPVOID*)& ToDownloadList);
-
 	// ==========================================================================
 	// SEARCH SECTION
 	// ==========================================================================
-	ifstream param("search_param.txt");
-	if (param.is_open())
-	{
-		wcout << L"Search criteria:" << endl;
-		while ( getline(param, str_param) )
-		{
-			cout << str_param << '\n';
-		}
-		param.close();
-	}
-	else 
-	{
-		wcout << "Unable to open file search_param.txt" << endl;
-		return 1;
-	}
-
-	const char* cstr = str_param.c_str();
-	int wslen = MultiByteToWideChar(CP_ACP, 0, cstr, strlen(cstr), 0, 0);
-	BSTR criteria = SysAllocStringLen(0, wslen);
-	MultiByteToWideChar(CP_ACP, 0, cstr, strlen(cstr), criteria, wslen);
-
+	criteria = getCriteria();
 	hr = CoCreateInstance(CLSID_UpdateSession, NULL, CLSCTX_INPROC_SERVER, IID_IUpdateSession, (LPVOID*)& sr.iUpdate);
 	hr = sr.iUpdate->CreateUpdateSearcher(&sr.searcher);
-
+	
 	wcout << L"Searching for updates ..." << endl;
 	hr = sr.searcher->Search(criteria, &sr.results);
-	
 	switch (hr)
 	{
 	case S_OK:
@@ -68,10 +45,10 @@ int main(int argc, _TCHAR* argv[])
 		return 0;
 	}
 	SysFreeString(criteria);
-	sr.results->get_Updates(&upd.List);
-	upd.List->get_Count(&upd.Size);
+	sr.results->get_Updates(&updates.UpdatesList);
+	updates.UpdatesList->get_Count(&updates.Size);
 	// Print updates info
-	PrintUInfo(upd);
+	PrintUInfo(updates, ToDownloadList);
 
 	// ==========================================================================
 	// DOWNLOAD SECTION
@@ -79,9 +56,6 @@ int main(int argc, _TCHAR* argv[])
 	hr = sr.iUpdate->CreateUpdateDownloader(&iDownloader);
 	switch(hr)
 	{
-	case S_OK:
-		wcout << L"Starting download ..." << endl;
-		break;
 	case E_INVALIDARG:
 		wcout << L"A parameter value is invalid" << endl;
 		return 0;
@@ -90,44 +64,9 @@ int main(int argc, _TCHAR* argv[])
 		return 0;
 	}
 
-	// Check cache
-	// get_IsDownloaded
-	for (LONG i = 0; i < upd.Size; i++)
-	{
-		long newIndex;
-		upd.List->get_Item(i, &upd.Item);
-		upd.Item->get_Title(&upd.Name);
-		hr = upd.Item->get_IsDownloaded(&upd.InCache);
-		switch (hr)
-		{
-		case S_OK:
-			if (upd.InCache) {
-				wcout << i + 1 << " - " << upd.Name << " Already downloaded" << endl;
-
-			}
-			else {
-				hr = ToDownloadList->Add(upd.Item, &newIndex);
-				switch (hr)
-				{
-				case E_POINTER:
-					wcout << L" A parameter value is invalid or NULL" << endl;
-					return 0;
-				case WU_E_NOT_SUPPORTED:
-					wcout << L"The collection is read-only" << endl;
-					return 0;
-				}
-			}
-			break;
-		default:
-			wcout << "Som error" << endl;
-			return 0;
-		}
-	}
-
 	// Updates to download
 	long tdLen;
 	ToDownloadList->get_Count(&tdLen);
-	wcout << tdLen << endl;
 	if (tdLen > 0) {
 		iDownloader->put_Updates(ToDownloadList);
 
@@ -140,17 +79,17 @@ int main(int argc, _TCHAR* argv[])
 			wcout << L"List of downloaded items on the machine:" << endl;
 			for (LONG i = 0; i < tdLen; i++)
 			{
-				ToDownloadList->get_Item(i, &upd.Item);
-				upd.Item->get_Title(&upd.Name);
+				ToDownloadList->get_Item(i, &updates.Item);
+				updates.Item->get_Title(&updates.Name);
 				IDResult->GetUpdateResult(i, &IUDResult);
-				hr = IUDResult->get_ResultCode(&upd.RC);
-				switch (upd.RC)
+				hr = IUDResult->get_ResultCode(&updates.RC);
+				switch (updates.RC)
 				{
 				case 2:
-					wcout << i + 1 << " - " << upd.Name << " Successfully downloaded" << endl;
+					wcout << i + 1 << " - " << updates.Name << " Successfully downloaded" << endl;
 					break;
 				default:
-					wcout << i + 1 << " - " << upd.Name << " RESULT: " << upd.RC << endl;
+					wcout << i + 1 << " - " << updates.Name << " RESULT: " << updates.RC << endl;
 					break;
 				}
 			}
@@ -175,20 +114,77 @@ int main(int argc, _TCHAR* argv[])
 	return 0;
 }
 
+// Get search criteria from file
+BSTR getCriteria() {
+	string str_param;
+	ifstream param("search_param.txt");
 
-void PrintUInfo(Updates upd) {
+	if (param.is_open())
+	{
+		wcout << L"Search criteria:" << endl;
+		while (getline(param, str_param))
+		{
+			cout << str_param << '\n';
+		}
+		param.close();
+	}
+	else
+	{
+		wcout << "Unable to open file search_param.txt" << endl;
+		return L"NULL";
+	}
+
+	const char* cstr = str_param.c_str();
+	int wslen = MultiByteToWideChar(CP_ACP, 0, cstr, strlen(cstr), 0, 0);
+	BSTR criteria = SysAllocStringLen(0, wslen);
+	MultiByteToWideChar(CP_ACP, 0, cstr, strlen(cstr), criteria, wslen);
+
+	return criteria;
+}
+
+// Print info about founded updates
+void PrintUInfo(Updates upd, IUpdateCollection* ToDownloadList) {
+	 HRESULT hr = CoInitialize(NULL);
+
 	if (upd.Size == 0)
 	{
 		wcout << L"No updates found" << endl;
+		return;
 	}
 
 	for (LONG i = 0; i < upd.Size; i++)
 	{
-		upd.List->get_Item(i, &upd.Item);
+		long newIndex;
+		upd.UpdatesList->get_Item(i, &upd.Item);
 		upd.Item->get_Title(&upd.Name);
 		upd.Item->get_LastDeploymentChangeTime(&upd.retdate);
 		COleDateTime odt;
 		odt.m_dt = upd.retdate;
-		wcout << i + 1 << " - " << upd.Name << "  Release Date " << (LPCTSTR)odt.Format(_T("%A, %B %d, %Y")) << endl;
+	
+		hr = upd.Item->get_IsDownloaded(&upd.InCache);
+		switch (hr)
+		{
+		case S_OK:
+			if (upd.InCache) {
+				wcout << i + 1 << " - " << upd.Name << "  Release Date " << (LPCTSTR)odt.Format(_T("%A, %B %d, %Y")) << " || Already downloaded" << endl;
+			}
+			else {
+				hr = ToDownloadList->Add(upd.Item, &newIndex);
+				wcout << i + 1 << " - " << upd.Name << "  Release Date " << (LPCTSTR)odt.Format(_T("%A, %B %d, %Y")) << " || To download" << endl;
+				switch (hr)
+				{
+				case E_POINTER:
+					wcout << L" A parameter value is invalid or NULL" << endl;
+					return;
+				case WU_E_NOT_SUPPORTED:
+					wcout << L"The collection is read-only" << endl;
+					return;
+				}
+			}
+			break;
+		default:
+			wcout << "Som error" << endl;
+			return;
+		}
 	}
 };
